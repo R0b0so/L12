8912f3f54c70ccc3e1521c77a75803a381b4b036
R0b0so <ethansagar222@gmail.com>
2024-10-30 19:52:02 -0700

    hello

---
+++ star.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(360,400)
+star = turtle.Turtle()
+
+star.forward(100)
+star.right(120)
+star.forward(100)
+star.right(120)
+star.forward(100)
+star.penup()
+star.right(120)
+star.forward(50)
+star.left(90)
+star.forward(30)
+star.pendown()
+star.right(150)
+star.forward(100)
+star.right(120)
+star.forward(100)
+star.right(120)
+star.forward(100)
+
+
+turtle.done()
